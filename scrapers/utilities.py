11423fa34e63b5e9bcea2d6a4596852d1fd33f6d
[Kene Nwogu] <[nwogujoseph1111@gmail.com]>
2022-05-06 21:01:35 +0100

    send as text

--- scrapers/utilities.py
+++ scrapers/utilities.py
@@ -32,7 +32,7 @@ def welcome_text(username):
     text += 'On selecting a valid sporty bet code, The current state of the ticket would be displayed. Then ' \
             'subsequent update messages would be broadcast to you. \n\n'
     text += 'Thank you for using this bot, Good luck to your games! ;)'
-    return escape_markdown(text)
+    return text
 
 
 def format_markdown_text(text):
